4e370bfb520eb512e2e9091ea1ff7de14acd31f0
Shivom Mishra <shivom_moalik@Shivoms-MacBook-Air.local>
2021-10-12 09:28:10 +0530

    file updated

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 function onRequest(request,response){
     response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write('Hi Nodejs demo');
+    response.write('Hi test');
     response.end();
 }
 
